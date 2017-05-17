2ce97ddb0931dcd62625afc4b402db9babc6b9d3
g9jadhav <Gandharva.Jadhav@capita.co.uk>
2017-05-17 23:06:40 +0530

    Added generic function to create dropdown. DD list binded with JSON data.

--- Excercise1/script.js
+++ Excercise1/script.js
@@ -1,10 +1,33 @@
 $(document).ready(function () {
     var JSONData = [];
+
+    //jQuery ajax to load JSON data from given URL
     $.ajax({
         url: "https://jsonplaceholder.typicode.com/posts",
         dataType: "json"
     }).done(function (data) {
-        console.log(data);
+        createNewDropDown(data, "container", "Please select id", "dropDown1");
     });
+
+    /**
+     * Function to simply create dropdown
+     */
+    function createNewDropDown(data, containerId, defaultText, ddId) {
+        var newSelect = document.createElement('select');
+        var selectHTML = "";
+        
+        //Specific condition for first dropdown, is it is having defaultText with "Please select" string
+        if (ddId == "dropDown1") {
+
+            selectHTML += "<option>" + defaultText + "</option>";
+
+        }
+        for (var j = 0; j < data.length; j++) {
+            selectHTML += "<option value='" + data[j].id + "'>" + data[j].id + "</option>";
+        }
+        newSelect.id = ddId;
+        newSelect.innerHTML = selectHTML;
+        $('#' + containerId).append(newSelect);
+    }
 
 });
